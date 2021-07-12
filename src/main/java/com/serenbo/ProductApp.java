dd7e61a1bb2040e95c9f7b987c29dcdd8bde3643
umuterdolu <Gr8onpil>
2021-07-13 01:37:40 +0300

    initial commit

---
+++ src/main/java/com/serenbo/ProductApp.java
@@ -0,0 +1,14 @@
+package com.serenbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Seren Bolat
+ */
+@SpringBootApplication
+public class ProductApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApp.class, args);
+    }
+}
---
+++ src/main/java/com/serenbo/exception/DataNotFoundException.java
@@ -0,0 +1,13 @@
+package com.serenbo.exception;
+
+/**
+ * @author Seren Bolat
+ */
+public class DataNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 7889764911297506503L;
+
+    public DataNotFoundException() {
+        super();
+    }
+}
